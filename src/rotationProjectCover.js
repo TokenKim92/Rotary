fdc0992a1d9ee661e7a861da4ad46c025acc5097
Token Kim <TokenKim92@gmail.com>
2022-08-02 00:55:27 +0200

    feature: implement a detail mode

--- src/rotationProjectCover.js
+++ src/rotationProjectCover.js
@@ -2,7 +2,7 @@ import { posInRect } from './utils.js';
 
 export default class RotationProjectCover {
   static DEGREE_INTERVAL = 10;
-  static FPS = 60;
+  static FPS = 30;
   static FPS_TIME = 1000 / RotationProjectCover.FPS;
   static WHEEL_UP = -1;
   static WHEEL_DOWN = 1;
@@ -13,6 +13,8 @@ export default class RotationProjectCover {
 
   #canvas;
   #ctx;
+  #canvasForMainCover;
+  #ctxForMainCover;
   #stageWidth;
   #stageHeight;
   #rotationAxisRadius;
@@ -26,20 +28,31 @@ export default class RotationProjectCover {
   #prevTime = 0;
   #prevSelectedIndex;
   #coverRects = [];
+  #prevRotateState = false;
 
   #filledBackgroundWidth = 0;
   #fillBackgroundSpeed;
   #fullscreenBtn;
+  #returnBtn;
+  #toBeFilled = false;
 
   constructor(projectCovers) {
     this.#fullscreenBtn = document.querySelector('.fullscreen');
+    this.#returnBtn = document.querySelector('.return');
+
     this.#canvas = document.createElement('canvas');
     this.#ctx = this.#canvas.getContext('2d');
     document.body.append(this.#canvas);
 
+    this.#canvasForMainCover = document.createElement('canvas');
+    this.#ctxForMainCover = this.#canvasForMainCover.getContext('2d');
+    document.body.append(this.#canvasForMainCover);
+
     window.addEventListener('resize', this.resize);
-    window.addEventListener('wheel', this.setTargetPerWheel);
+    //window.addEventListener('wheel', this.setTargetPerWheel);
     window.addEventListener('click', this.onClickCoverItem);
+    this.#fullscreenBtn.addEventListener('click', () => (this.#toBeFilled = true)); // prettier-ignore
+    this.#returnBtn.addEventListener('click', this.returnToMainStage);
 
     this.onWebFontLoad(projectCovers);
   }
@@ -66,12 +79,22 @@ export default class RotationProjectCover {
     this.resize();
   };
 
+  returnToMainStage = () => {
+    this.#ctx.clearRect(0, 0, this.#stageWidth, this.#stageHeight);
+    this.drawCoverItems();
+    this.#scaleSelectedCover({ x: 1.1, y: 1.1 });
+    this.#returnBtn.style.display = 'none';
+  };
+
   resize = () => {
     this.#stageWidth = document.body.clientWidth;
     this.#stageHeight = document.body.clientHeight;
 
     this.#canvas.width = this.#stageWidth;
     this.#canvas.height = this.#stageHeight;
+
+    this.#canvasForMainCover.width = this.#stageWidth;
+    this.#canvasForMainCover.height = this.#stageHeight;
 
     this.#fillBackgroundSpeed =
       this.#stageHeight / RotationProjectCover.FPS_TIME;
@@ -83,6 +106,7 @@ export default class RotationProjectCover {
     };
 
     this.drawCoverItems();
+    this.#scaleSelectedCover({ x: 1.1, y: 1.1 });
 
     window.requestAnimationFrame(this.animate);
   };
@@ -169,7 +193,7 @@ export default class RotationProjectCover {
     });
   }
 
-  drawCover(cover, rotationPos, radian = 0) {
+  drawCover(cover, rotationPos, radian) {
     this.#ctx.save();
 
     this.#ctx.translate(rotationPos.x, rotationPos.y);
@@ -179,7 +203,7 @@ export default class RotationProjectCover {
     this.#ctx.restore();
   }
 
-  drawTitle(cover, rotationPos, radian = 0) {
+  drawTitle(cover, rotationPos, radian) {
     this.#ctx.save();
 
     const textRadian = (270 * Math.PI) / 180;
@@ -199,28 +223,47 @@ export default class RotationProjectCover {
   }
 
   animate = (curTime) => {
-    this.#checkFPSTime(curTime);
+    if (this.#isRotating()) {
+      this.#ctx.clearRect(0, 0, this.#stageWidth, this.#stageHeight);
+      this.drawCoverItems();
+
+      if (!this.#prevRotateState) {
+        this.#ctxForMainCover.clearRect(0, 0, this.#stageWidth, this.#stageHeight); // prettier-ignore
+
+        this.#prevRotateState = true;
+      }
+    } else {
+      if (this.#prevRotateState) {
+        this.#scaleSelectedCover({ x: 1.1, y: 1.1 });
+
+        this.#prevRotateState = false;
+      }
+    }
+
+    this.#toBeFilled && this.#fillBackground();
 
     window.requestAnimationFrame(this.animate);
   };
 
-  #checkFPSTime(curTime) {
-    if (!this.#prevTime) {
-      this.#prevTime = curTime;
-    }
+  #scaleSelectedCover(ratio) {
+    const degree =
+      RotationProjectCover.DEGREE_INTERVAL * this.#prevSelectedIndex -
+      this.#currentDegree;
+    const radian = (degree * Math.PI) / 180;
 
-    if (curTime - this.#prevTime > RotationProjectCover.FPS_TIME) {
-      this.#prevTime = curTime;
+    const rotationPos = {
+    x: this.#rotationAxisPos.x + this.#rotationAxisRadius * Math.sin(radian),
+    y: this.#rotationAxisPos.y - this.#rotationAxisRadius * Math.cos(radian)  
+  } // prettier-ignore
 
-      this.#onFPSTime();
-    }
-  }
+    this.#ctxForMainCover.save();
 
-  #onFPSTime() {
-    if (this.#isRotating()) {
-      this.#ctx.clearRect(0, 0, this.#stageWidth, this.#stageHeight);
-      this.drawCoverItems();
-    }
+    this.#ctxForMainCover.clearRect(0, 0, this.#stageWidth, this.#stageHeight);
+    this.#ctxForMainCover.translate(rotationPos.x, rotationPos.y);
+    this.#ctxForMainCover.scale(ratio.x, ratio.y);
+    this.#projectCovers[this.#prevSelectedIndex].animate(this.#ctxForMainCover);
+
+    this.#ctxForMainCover.restore();
   }
 
   #isRotating() {
@@ -234,5 +277,19 @@ export default class RotationProjectCover {
     }
 
     return false;
+  }
+
+  #fillBackground() {
+    this.#filledBackgroundWidth += this.#fillBackgroundSpeed;
+
+    this.#ctx.fillStyle = 'black';
+    this.#ctx.fillRect(0, 0, this.#filledBackgroundWidth, this.#stageHeight);
+
+    if (this.#filledBackgroundWidth >= this.#stageWidth) {
+      this.#scaleSelectedCover({ x: 2, y: 2 });
+      this.#returnBtn.style.display = 'block';
+      this.#filledBackgroundWidth = 0;
+      this.#toBeFilled = false;
+    }
   }
 }
